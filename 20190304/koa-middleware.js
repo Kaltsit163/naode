fe44a36dceb8fec5dad2a23f0d1c00e15560b820
xiaolin <xiaolin@in66.com>
2019-03-04 23:33:58 +0800

    curd

--- 20190304/koa-middleware.js
+++ 20190304/koa-middleware.js
@@ -5,24 +5,33 @@ const bodyParser = require('koa-body');
 const app = new Koa();
 const router = new Router();
 
-let users = [];
+let users = {};
 
 app.use(bodyParser());
 
 router
-  .get('/user', async (ctx, next) => {
+  .get('/user/:id/data', async (ctx, next) => {
+    const id = ctx.params.id;
     ctx.body = {
-      data: users
-    }
-  }).post('/user', async (ctx, next) => {
-    const { user } = ctx.request.body;
-    users.push(user);
+      data: users[id] || {}
+    };
+  }).post('/user/:id/data', async (ctx, next) => {
+    const id = ctx.params.id;
+    const { data } = ctx.request.body;
+    users[id] = data;
     ctx.body = {
-      data: users
+      data: users[id] || {}
     }
-  }).del('/user')
+  }).del('/user/:id/data', async (ctx, next) => {
+    const id = ctx.params.id;
+    users[id] = {};
+    ctx.body = {
+      data: users[id] || {}
+    }
+  });
 
 app.use(router.routes());
 
 app.listen(6666);
 
+console.log('6666 is on');
